e36533c2d9c1083b4b664909950f3ca8b29a4497
TheOnlyKirb <nicoloffaj@g.cofc.edu>
2021-02-18 14:57:26 -0500

    revert, i dont need the fancy

--- Client/functions/cacheServer.js
+++ Client/functions/cacheServer.js
@@ -1,4 +1,5 @@
-module.exports = async function cacheServer(Client, data) {
+
+module.exports = function cacheServer(Client, data) {
     Client.music.servers.set(data.guild.id, {
         guild: data.guild,
         spawnedChannel: data.summoned,
@@ -6,19 +7,6 @@ module.exports = async function cacheServer(Client, data) {
         vc: data.vc,
         dispatcher: data.dispatcher,
         player: data.player,
-        userControl: data.user,
-        args: { // so i dont really need to add this but im going to bc im bored
-            bass: 0,
-            tempo: 1,
-            treble: 0,
-            custom: ""
-        },
-        buffer: { // each server will have its own buffer/opus stream- this allows equalizer settings via ffmpeg.
-            game: null,
-            all: null,
-            ocremix: null,
-            covers: null,
-            chiptune: null
-        }
+        userControl: data.user
     })
 }
--- Client/relay/sweep.js
+++
@@ -1,3 +0,0 @@
-module.exports = async function bufferSweep(buffer) {
-    console.log(buffer)
-}
